0ad2cd97f5246aede24c6027e091faab43bbe778
khorshid <khorshid.b06@gmail.com>
2025-08-09 14:34:04 +0330

    ,,

--- core/src/main/java/com/P/common/model/enums/ForAgingSeeds.java
+++ core/src/main/java/com/P/common/model/enums/ForAgingSeeds.java
@@ -1,78 +1,88 @@
 package com.P.common.model.enums;
 
+import com.P.Client.model.GameAssetManager;
+import com.P.common.model.Basics.Game;
+import com.badlogic.gdx.graphics.Texture;
+
 public enum ForAgingSeeds {
 
-    jazzSeeds("Jazz Seeds", new Season[]{Season.SPRING}, 30),
-    carrotSeeds("Carrot Seeds", new Season[]{Season.SPRING}, 4),
-    cauliflowerSeeds("Cauliflower Seeds", new Season[]{Season.SPRING}, 80),
-    coffeeBean("Coffee Bean", new Season[]{Season.SPRING}, 160),
-    garlicSeeds("Garlic Seeds", new Season[]{Season.SPRING}, 40),
-    beanStarter("Bean Starter", new Season[]{Season.SPRING}, 60),
-    kaleSeeds("Kale Seeds", new Season[]{Season.SPRING}, 70),
-    parsnipSeeds("Parsnip Seeds", new Season[]{Season.SPRING}, 20),
-    potatoSeeds("Potato Seeds", new Season[]{Season.SPRING}, 50),
-    rhubarbSeeds("Rhubarb Seeds", new Season[]{Season.SPRING}, 80),
-    strawberrySeeds("Strawberry Seeds", new Season[]{Season.SPRING}, 80),
-    tulipBulb("Tulip Bulb", new Season[]{Season.SPRING}, 20),
-    riceShoot("Rice Shoot", new Season[]{Season.SPRING}, 40),
+    jazzSeeds("Jazz Seeds", new Season[]{Season.SPRING}, 30, GameAssetManager.JazzSeeds),
+    carrotSeeds("Carrot Seeds", new Season[]{Season.SPRING}, 4, GameAssetManager.CarrotSeeds),
+    cauliflowerSeeds("Cauliflower Seeds", new Season[]{Season.SPRING}, 80, GameAssetManager.CauliflowerSeeds),
+    coffeeBean("Coffee Bean", new Season[]{Season.SPRING}, 160,GameAssetManager.CoffeeBean ),
+    garlicSeeds("Garlic Seeds", new Season[]{Season.SPRING}, 40, GameAssetManager.GarlicSeeds),
+    beanStarter("Bean Starter", new Season[]{Season.SPRING}, 60, GameAssetManager.BeanStarter),
+    kaleSeeds("Kale Seeds", new Season[]{Season.SPRING}, 70, GameAssetManager.KaleSeeds),
+    parsnipSeeds("Parsnip Seeds", new Season[]{Season.SPRING}, 20,GameAssetManager.ParsnipSeeds ),
+    potatoSeeds("Potato Seeds", new Season[]{Season.SPRING}, 50, GameAssetManager.PotatoSeeds),
+    rhubarbSeeds("Rhubarb Seeds", new Season[]{Season.SPRING}, 80, GameAssetManager.RhubarbSeeds),
+    strawberrySeeds("Strawberry Seeds", new Season[]{Season.SPRING}, 80, GameAssetManager.StrawberrySeeds),
+    tulipBulb("Tulip Bulb", new Season[]{Season.SPRING}, 20, GameAssetManager.TulipBulb),
+    riceShoot("Rice Shoot", new Season[]{Season.SPRING}, 40, GameAssetManager.RiceShoot),
 
-    blueberrySeeds("Blueberry Seeds", new Season[]{Season.SUMMER}, 80),
-    cornSeeds("Corn Seeds", new Season[]{Season.SUMMER}, 150),
-    hopsStarter("Hops Starter", new Season[]{Season.SUMMER}, 60),
-    pepperSeeds("Pepper Seeds", new Season[]{Season.SUMMER}, 40),
-    melonSeeds("Melon Seeds", new Season[]{Season.SUMMER}, 80),
-    poppySeeds("Poppy Seeds", new Season[]{Season.SUMMER}, 100),
-    radishSeeds("Radish Seeds", new Season[]{Season.SUMMER}, 40),
-    redCabbageSeeds("Red Cabbage Seeds", new Season[]{Season.SUMMER}, 100),
-    starfruitSeeds("Starfruit Seeds", new Season[]{Season.SUMMER}, 320),
-    spangleSeeds("Spangle Seeds", new Season[]{Season.SUMMER}, 50),
-    summerSquashSeeds("Summer Squash Seeds", new Season[]{Season.SUMMER}, 8),
-    sunflowerSeeds("Sunflower Seeds", new Season[]{Season.SUMMER}, 200),
-    tomatoSeeds("Tomato Seeds", new Season[]{Season.SUMMER}, 50),
-    wheatSeeds("Wheat Seeds", new Season[]{Season.SUMMER}, 10),
+    blueberrySeeds("Blueberry Seeds", new Season[]{Season.SUMMER}, 80,GameAssetManager.BlueberrySeeds ),
+    cornSeeds("Corn Seeds", new Season[]{Season.SUMMER}, 150, GameAssetManager.CornSeeds),
+    hopsStarter("Hops Starter", new Season[]{Season.SUMMER}, 60, GameAssetManager.HopsStarter),
+    pepperSeeds("Pepper Seeds", new Season[]{Season.SUMMER}, 40,GameAssetManager.PepperSeeds ),
+    melonSeeds("Melon Seeds", new Season[]{Season.SUMMER}, 80, GameAssetManager.MelonSeeds),
+    poppySeeds("Poppy Seeds", new Season[]{Season.SUMMER}, 100, GameAssetManager.PoppySeeds),
+    radishSeeds("Radish Seeds", new Season[]{Season.SUMMER}, 40, GameAssetManager.RadishSeeds),
+    redCabbageSeeds("Red Cabbage Seeds", new Season[]{Season.SUMMER}, 100, GameAssetManager.RedCabbageSeeds),
+    starfruitSeeds("Starfruit Seeds", new Season[]{Season.SUMMER}, 320, GameAssetManager.StarfruitSeeds),
+    spangleSeeds("Spangle Seeds", new Season[]{Season.SUMMER}, 50, GameAssetManager.SpangleSeeds),
+    summerSquashSeeds("Summer Squash Seeds", new Season[]{Season.SUMMER}, 8, GameAssetManager.SummerSquashSeeds),
+    sunflowerSeeds("Sunflower Seeds", new Season[]{Season.SUMMER}, 200, GameAssetManager.SunflowerSeeds),
+    tomatoSeeds("Tomato Seeds", new Season[]{Season.SUMMER}, 50, GameAssetManager.TomatoSeeds),
+    wheatSeeds("Wheat Seeds", new Season[]{Season.SUMMER}, 10, GameAssetManager.WheatSeeds),
 
-    amaranthSeeds("Amaranth Seeds", new Season[]{Season.AUTUMN}, 70),
-    artichokeSeeds("Artichoke Seeds", new Season[]{Season.AUTUMN}, 30),
-    beetSeeds("Beet Seeds", new Season[]{Season.AUTUMN}, 16),
-    bokChoySeeds("Bok Choy Seeds", new Season[]{Season.AUTUMN}, 50),
-    broccoliSeeds("Broccoli Seeds", new Season[]{Season.AUTUMN}, 12),
-    cranberrySeeds("Cranberry Seeds", new Season[]{Season.AUTUMN}, 240),
-    eggplantSeeds("Eggplant Seeds", new Season[]{Season.AUTUMN}, 150),
-    fairySeeds("Fairy Seeds", new Season[]{Season.AUTUMN}, 200),
-    grapeStarter("Grape Starter", new Season[]{Season.AUTUMN}, 60),
-    pumpkinSeeds("Pumpkin Seeds", new Season[]{Season.AUTUMN}, 100),
-    yamSeeds("Yam Seeds", new Season[]{Season.AUTUMN}, 60),
-    rareSeed("Rare Seed", new Season[]{Season.AUTUMN}, 800),
+    amaranthSeeds("Amaranth Seeds", new Season[]{Season.AUTUMN}, 70, GameAssetManager.AmaranthSeeds),
+    artichokeSeeds("Artichoke Seeds", new Season[]{Season.AUTUMN}, 30, GameAssetManager.ArtichokeSeeds),
+    beetSeeds("Beet Seeds", new Season[]{Season.AUTUMN}, 16, GameAssetManager.BeetSeeds),
+    bokChoySeeds("Bok Choy Seeds", new Season[]{Season.AUTUMN}, 50, GameAssetManager.BokChoySeeds),
+    broccoliSeeds("Broccoli Seeds", new Season[]{Season.AUTUMN}, 12, GameAssetManager.BroccoliSeeds),
+    cranberrySeeds("Cranberry Seeds", new Season[]{Season.AUTUMN}, 240, GameAssetManager.CranberrySeeds),
+    eggplantSeeds("Eggplant Seeds", new Season[]{Season.AUTUMN}, 150, GameAssetManager.EggplantSeeds),
+    fairySeeds("Fairy Seeds", new Season[]{Season.AUTUMN}, 200, GameAssetManager.FairySeeds),
+    grapeStarter("Grape Starter", new Season[]{Season.AUTUMN}, 60, GameAssetManager.GrapeStarter),
+    pumpkinSeeds("Pumpkin Seeds", new Season[]{Season.AUTUMN}, 100, GameAssetManager.PumpkinSeeds),
+    yamSeeds("Yam Seeds", new Season[]{Season.AUTUMN}, 60, GameAssetManager.YamSeeds),
+    rareSeed("Rare Seed", new Season[]{Season.AUTUMN}, 800, GameAssetManager.RareSeed),
 
-    powdermelonSeeds("Powdermelon Seeds", new Season[]{Season.WINTER}, 20),
+    powdermelonSeeds("Powdermelon Seeds", new Season[]{Season.WINTER}, 20, GameAssetManager.PowdermelonSeeds),
 
-    ancientSeeds("Ancient Seeds", new Season[]{Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER}, 400),
-    mixedSeeds("Mixed Seeds", new Season[]{Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER}, 0),
+    ancientSeeds("Ancient Seeds", new Season[]{Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER}, 400, GameAssetManager.AncientSeeds),
+    mixedSeeds("Mixed Seeds", new Season[]{Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER}, 0, GameAssetManager.MixedSeeds),
 
-    apricotSapling("apricot sapling", new Season[]{Season.SPRING}, 0),
-    cherrySapling("cherry sapling", new Season[]{Season.SPRING}, 0),
-    bananaSapling("banana sapling", new Season[]{Season.SUMMER}, 0),
-    mangoSapling("mango sapling", new Season[]{Season.SUMMER}, 0),
-    orangeSapling("orange sapling", new Season[]{Season.SUMMER}, 0),
-    peachSapling("peach sapling", new Season[]{Season.SUMMER}, 0),
-    appleSapling("apple sapling", new Season[]{Season.AUTUMN}, 0),
-    pomegranateSapling("pomegranate sapling", new Season[]{Season.AUTUMN}, 0),
-    acorns("acorns", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
-    mapleSeeds("maple seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
-    pineCones("pine cones", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
-    mahoganySeeds("mahogany seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
-    mushroomTreeSeeds("mushroom tree seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
-    mysticTreeSeeds("mystic tree seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0),
+    apricotSapling("apricot sapling", new Season[]{Season.SPRING}, 0,GameAssetManager.ApricotSapling),
+    cherrySapling("cherry sapling", new Season[]{Season.SPRING}, 0, GameAssetManager.CherrySapling),
+    bananaSapling("banana sapling", new Season[]{Season.SUMMER}, 0,GameAssetManager.BananaSapling ),
+    mangoSapling("mango sapling", new Season[]{Season.SUMMER}, 0, GameAssetManager.MangoSapling),
+    orangeSapling("orange sapling", new Season[]{Season.SUMMER}, 0, GameAssetManager.OrangeSapling),
+    peachSapling("peach sapling", new Season[]{Season.SUMMER}, 0, GameAssetManager.PeachSapling),
+    appleSapling("apple sapling", new Season[]{Season.AUTUMN}, 0, GameAssetManager.AppleSapling),
+    pomegranateSapling("pomegranate sapling", new Season[]{Season.AUTUMN}, 0, GameAssetManager.PomegranateSapling),
+    acorns("acorns", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0,GameAssetManager.Acorns ),
+    mapleSeeds("maple seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0, GameAssetManager.MapleSeeds),
+    pineCones("pine cones", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0, GameAssetManager.PineCones),
+    mahoganySeeds("mahogany seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0, GameAssetManager.MahoganySeeds),
+    mushroomTreeSeeds("mushroom tree seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0,GameAssetManager.MushroomTreeSeeds ),
+    mysticTreeSeeds("mystic tree seeds", new Season[]{Season.AUTUMN, Season.SPRING, Season.WINTER, Season.SUMMER}, 0,GameAssetManager.MysticTreeSeeds ),
     ;
     private final String seedName;
     private final Season[] season;
     private final int price;
+    private final Texture texture;
 
 
-    ForAgingSeeds(String seedName, Season[] season, int price) {
+    ForAgingSeeds(String seedName, Season[] season, int price, Texture texture) {
         this.seedName = seedName;
         this.season = season;
         this.price = price;
+        this.texture = texture;
+    }
+
+    public Texture getTexture() {
+        return texture;
     }
 
     public String getSeedName() {
